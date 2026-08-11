49d14b66145c095ca6e9f47b4f3a1b2b6e69928e
Chocobe <kyw05171@gmail.com>
2026-08-11 21:19:50 +0900

    환경별 application.yaml 작성 및 H2 설정 - application.yaml : 공통 설정 - application-dev.yaml : 개발 환경 - application-test.yaml : 유닛 테스트 환경

--- src/test/java/com/github/chocobe/p62mission1260730/P62Mission1260730ApplicationTests.java
+++ src/test/java/com/github/chocobe/p62mission1260730/P62Mission1260730ApplicationTests.java
@@ -2,12 +2,18 @@ package com.github.chocobe.p62mission1260730;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "test")
 class P62Mission1260730ApplicationTests {
 
     @Test
     void contextLoads() {
+        boolean truthy = true;
+        assertTrue(truthy);
     }
 
 }
