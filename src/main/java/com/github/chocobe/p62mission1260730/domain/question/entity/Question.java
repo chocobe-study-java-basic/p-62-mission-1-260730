78ad55fd985792d59d27cc0906ec8fbd3f0c56d5
Chocobe <kyw05171@gmail.com>
2026-08-11 21:29:45 +0900

    Question Entity 작성

---
+++ src/main/java/com/github/chocobe/p62mission1260730/domain/question/entity/Question.java
@@ -0,0 +1,26 @@
+package com.github.chocobe.p62mission1260730.domain.question.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+}
